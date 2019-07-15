90580c2e1e7cf9269cbe05c1a0c3a21bf41ddca4
sawepeter <sawepeter6@gmail.com>
2019-07-15 11:20:38 +0300

    intial commit

--- app/src/main/java/com/example/alcchallenge/MainActivity.java
+++ app/src/main/java/com/example/alcchallenge/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.alcchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button profile,about_alc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize views
+        about_alc = findViewById(R.id.about_alc);
+        profile = findViewById(R.id.profile);
+
+        //setting onclick listeners
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        about_alc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
